3349473c022a8032949e6d2ebf3f1cd9dc6258ba
Nguyen Anh <anhn@asiantech.vn>
2015-08-18 16:53:33 +0700

    First commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var time = 0;
+var end =  0
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+  var newfile = fs.createWriteStream('myfile.txt');
+  var fileLength = req.headers['content-length'];
+  var uploadedBytes = 0;
+  time = new Date().getTime();
+  req.pipe(newfile);
+
+   req.on('data', function(chunk) {
+      uploadedBytes += chunk.length;
+      var progress = uploadedBytes / fileLength * 100; // Phần trăm upload file.
+      console.log('Uploaded: ' + parseInt(progress, 10) + '% \n');
+  });
+
+   req.on('end', function() {
+      console.log(new Date().getTime() - time);
+      console.log('Your file was uploaded successful!');
+      res.sendStatus(new Date().getTime() - time);
+   });
+});
+module.exports = router;
